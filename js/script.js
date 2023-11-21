740d3643828b4311556781c1966539844b3bbe98
alex02r <alex.giochi02@gmail.com>
2023-11-21 14:54:10 +0100

    [UPGRADE]Add funtion to calcolate the train number

--- js/script.js
+++ js/script.js
@@ -6,8 +6,18 @@ invia.addEventListener('click', function ( ){
     let km = document.getElementById('km').value;
     let eta = document.getElementById('eta').value;
 
+    //dati da inserire nel bliglietto 
+    let name = document.getElementById('name').value;
+
     //calcoliamo il prezzo
     let prezzo = km * 0.21;
+    
+    //calcoliamo il numero del treno (random)
+    let cifra1 = Math.floor(Math.random() * 10);
+    let cifra2 = Math.floor(Math.random() * 10);
+    let cifra3 = Math.floor(Math.random() * 10);
+    let cifra4 = Math.floor(Math.random() * 10);
+    let train = `${cifra1}${cifra2}${cifra3}${cifra4}`;
 
     //eseguiamo il controllo dell'età
     if(eta < 18){
@@ -17,5 +27,6 @@ invia.addEventListener('click', function ( ){
         //l'utente ha più di 65 anni e va applicato lo sconto del 40%
         prezzo = prezzo * 0.6;
     }
+    console.log(train);
     console.log(prezzo.toFixed(2));
 })
