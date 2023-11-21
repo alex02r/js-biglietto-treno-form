b7ce94e040112f0e1b9dffec566d8f969f9cacfd
alex02r <alex.giochi02@gmail.com>
2023-11-21 13:50:57 +0100

    [UPGRADE]Set on click the button of form

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Selezioniamo il bottone che invia i dati al form
+let invia = document.getElementById('invia');
+//Al click del bottone svolgiamo le "operazioni"
+invia.addEventListener('click', function ( ){
+    //siamo qui perchè abbiamo cliccato invia
+    let km = document.getElementById('km').value;
+    let eta = document.getElementById('eta').value;
+})
