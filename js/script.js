580dd642add69ecd56c8720b022e5133c623923b
alex02r <alex.giochi02@gmail.com>
2023-11-21 14:54:10 +0100

    [UPGRADE]Add type of bid

--- js/script.js
+++ js/script.js
@@ -6,16 +6,37 @@ invia.addEventListener('click', function ( ){
     let km = document.getElementById('km').value;
     let eta = document.getElementById('eta').value;
 
+    //dati da inserire nel bliglietto 
+    let name = document.getElementById('name').value;
+
     //calcoliamo il prezzo
     let prezzo = km * 0.21;
 
+    //calcoliamo il numero del treno (random)
+    let cifra1 = Math.floor(Math.random() * 10);
+    let cifra2 = Math.floor(Math.random() * 10);
+    let cifra3 = Math.floor(Math.random() * 10);
+    let cifra4 = Math.floor(Math.random() * 10);
+    let train = `${cifra1}${cifra2}${cifra3}${cifra4}`;
+
+    //variabile per scrivere sul biglietto che tipo di offerta abbiamo
+    let offerta = "Standard";
+
+    
     //eseguiamo il controllo dell'età
     if(eta < 18){
         //l'utente è minorenne e va applicato lo sconto del 20%
         prezzo = prezzo * 0.8; 
+
+        //cambiamo il tipo di offerta il tipo di offerta
+        offerta = "Under 18 -20%";
     }else if (eta > 65) {
         //l'utente ha più di 65 anni e va applicato lo sconto del 40%
         prezzo = prezzo * 0.6;
+        //cambiamo il tipo di offerta il tipo di offerta
+        offerta = "Over 65 -40%";
     }
+    console.log(offerta);
+    console.log(train);
     console.log(prezzo.toFixed(2));
 })
